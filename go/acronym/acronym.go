60ed8bad80daab76dde321e22066add65dfae3b8
Anton <prokopovich@vistrobotics.ai>
2020-09-22 12:35:11 +0300

    fix for dashes and underscores

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -3,8 +3,10 @@ package acronym
 import "strings"
 
 // Abbreviate converts a phrase to its acronym.
-func Abbreviate(s string) string {
-	var res string
+func Abbreviate(s string) (res string) {
+
+	s = strings.Replace(s, "-", " ", -1)
+	s = strings.Replace(s, "_", " ", -1)
 
 	for _, w := range strings.Fields(s) {
 		res = res + strings.ToUpper(w[:1])
